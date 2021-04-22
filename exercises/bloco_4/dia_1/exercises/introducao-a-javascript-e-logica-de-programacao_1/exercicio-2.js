f77ddd5547767c560debe510a4018352e60734cc
gscuri <gscuri6@hotmail.com>
2021-04-22 20:37:59 -0300

    Exercicio 2 maior entre dois numeros

---
+++ exercises/bloco_4/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio-2.js
@@ -0,0 +1,12 @@
+// 2 -Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+let valor1 = 7;
+let valor2 = 23;
+
+if (valor1 > valor2) {
+  console.log(' Valor1 é o maior numero');
+} else if (valor1 < valor2) {
+  console.log('Valor 2 é o maior numero');
+} else {
+  console.log('Numeros iguais');
+}
