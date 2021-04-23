5ff2844f802ac1d3fa95196f6f1c0d53a066c972
gscuri <gscuri6@hotmail.com>
2021-04-22 21:06:10 -0300

    ex 4 retuns if variable is posite, negative or zero

--- exercises/bloco_4/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio-3.js
+++ exercises/bloco_4/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio-3.js
@@ -1,6 +1,6 @@
 // 3- Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
-let valor1 = 7;
-let valor2 = 23;
+let valor1 = 79;
+let valor2 = 152;
 let valor3 = 78;
 
 if (valor1 > valor2 && valor1 > valor3) {
---
+++ exercises/bloco_4/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio-4.js
@@ -0,0 +1,10 @@
+// 4- Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+let VariavelPNZ = -235
+;
+
+if (VariavelPNZ > 0) {
+  console.log("positive");
+} else if (VariavelPNZ < 0) {
+  console.log("negative");
+} else console.log("zero");
