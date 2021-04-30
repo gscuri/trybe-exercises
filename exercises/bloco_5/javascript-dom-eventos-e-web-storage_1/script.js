39c3559faa1820c4f79a91ba42923abb1bd59736
gscuri <gscuri6@hotmail.com>
2021-04-29 21:10:47 -0300

    4 primiros exercicios, faltam colocar todos em funcao

---
+++ exercises/bloco_5/javascript-dom-eventos-e-web-storage_1/script.js
@@ -0,0 +1,11 @@
+function name(params) {
+    document.getElementsByTagName("p")[0].innerHTML= "Daqui dois anos pretendo estar vivendo a liberdade de trabalho que escolhi pela area"
+    
+}
+
+document.getElementsByClassName("main-content")[0].style= "background-color: rgb(76,164,109)"
+
+document.getElementsByClassName("center-content")[0].style= "background-color: white"
+
+document.getElementsByTagName("h1")[0].innerText= "Exercicio 5.1 - Javascript"
+
